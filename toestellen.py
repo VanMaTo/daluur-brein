2c1da0cb4d422e528f12580c5168092d557cc91d
VanMaTo <mvanherf@gmail.com>
2026-06-26 21:22:31 +0200

    Change 'actief' status of warmtepomp to False

--- toestellen.py
+++ toestellen.py
@@ -43,7 +43,7 @@ TOESTELLEN = [
     {
         "naam": "warmtepomp",
         "id": "bff1ab1d1427ccdd9ag0z4",
-        "actief": True,
+        "actief": False,
         "volgorde": 3,
         "schakelcode": "switch",      # bevestigd uit de status
         "meet_verbruik": False,
